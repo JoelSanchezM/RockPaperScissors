05a45efbc7bef5057e1d764f3f8ffe9d7411a2b6
Joel Sanchez <joel.sanchez.moran@gmail.com>
2022-08-04 00:28:31 -0500

    Create function getComputerChoice
    This function returns the computer choice via a random integer number
    from 0-2.

---
+++ main.js
@@ -0,0 +1,11 @@
+function getComputerChoice(){
+    /* 
+    Returns computer choice.
+    0 = Paper
+    1 = Rock 
+    2 = Scissors
+    */
+    return Math.floor(Math.random()*3)
+}
+
+getComputerChoice()
