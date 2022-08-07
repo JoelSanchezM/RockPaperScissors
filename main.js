17d558c02f66ce19c36b8b0df424b20cb5cc2f8c
Joel Sanchez <joel.sanchez.moran@gmail.com>
2022-08-06 20:03:49 -0500

    Create function playRound

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 function getComputerChoice(){
-    const options = ['Paper', 'Rock', 'Scissors']
+    const options = ['paper', 'rock', 'scissors']
     return options[Math.floor(Math.random()*3)]
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase()
+    let result = `Player selection: ${playerSelection}\n`
+    result += `Computer selection: ${computerSelection}\n`
+    
+    if (playerSelection == 'paper'){
+        if (computerSelection == 'paper') result += 'Tie!'
+        else if (computerSelection == 'rock') result += 'Winner: Player'
+        else result += 'Winner: Computer'
+    }
+    else if (playerSelection == 'rock'){
+        if (computerSelection == 'paper') result += 'Winner: Computer'
+        else if (computerSelection == 'rock') result += 'Tie!'
+        else result += 'Winner: Player'
+    }
+    else if (playerSelection == 'scissors'){
+        if (computerSelection == 'paper') result += 'Winner: Player'
+        else if (computerSelection == 'rock') result += 'Winner: Computer'
+        else result += 'Tie!'
+    }
+    else result = ('Error: Invalid choice!')
+
+    return result
+}
+const playerSelection = 'rock'
+const computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
