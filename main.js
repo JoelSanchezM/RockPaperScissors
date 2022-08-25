9a925ed74a131fd5f7c2db2c307c5758b7c624c5
Joel Sanchez <joel.sanchez.moran@gmail.com>
2022-08-24 23:43:45 -0500

    Change images when player makes a choice

--- main.js
+++ main.js
@@ -40,6 +40,10 @@ function game(choice){
 buttons = document.querySelectorAll('.buttons > button')
 scoreboard = document.querySelector('.scoreboard')
 message = document.querySelector('.message')
+playerSelectionImg = document.querySelector('.player__selections > img')
+computerSelectionImg = document.querySelector('.computer__selections > img')
+
+
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -51,7 +55,10 @@ buttons.forEach(button => {
         scoreboard.innerHTML = `<p>Scores</p>
         <p>Player: ${playerWins}</p>
         <p>Computer: ${computerWins}</p>`
-        message.innerText = `Winner: ${result[2]}`
+        playerSelectionImg.src = `./imgs/${result[0]}.jpg`
+        computerSelectionImg.src = `./imgs/${result[1]}.jpg`
+        if (result[2] == 'Tie') message.innerText = 'Tie!'
+        else message.innerText = `Winner: ${result[2]}`
         
     })
 
